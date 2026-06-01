5938db13749bf59051ec3e01ac4ed10edc7c1a8f
Parthasarathy <parthasarathysamikkannu@gmail.com>
2026-06-01 12:58:58 +0530

    added mult

--- 07_00_NN.py
+++ 07_00_NN.py
@@ -4,6 +4,16 @@ class Value:
     
     def __repr__(self):
         return f"Value(data={self.data})"
+    
+    def __add__(self, other):
+        return Value(self.data + other.data)
+    
+    def __mul__(self, other):
+        return Value(self.data * other.data)
 
-val = Value(3)
-print(val)
+a = Value(2)
+b = Value(-3)
+c = Value (10)
+
+print (a * b + c)
+print((a.__mul__(b)).__add__(c))
